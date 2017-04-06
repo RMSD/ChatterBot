17fc24667f8f9539c711b09c92167ca1bb3aaa2c
RMSD <rangers1500@yahoo.com>
2017-04-06 15:59:04 -0700

    Updated views at server post
    Updated the views at server post to create trakerr events upon certain user inputs.

--- chatterbot/ext/django_chatterbot/views.py
+++ chatterbot/ext/django_chatterbot/views.py
@@ -1,9 +1,17 @@
+"""
+Modified as a free demo for trakerr.
+"""
+
 import json
-from django.views.generic import View
+import string
+
 from django.http import JsonResponse
+from django.views.generic import View
+
 from chatterbot import ChatBot
 from chatterbot.ext.django_chatterbot import settings
 
+from trakerr import TrakerrClient
 
 class ChatterBotViewMixin(object):
     """
@@ -64,6 +72,49 @@ class ChatterBotView(ChatterBotViewMixin, View):
 
         chat_session = self.get_chat_session(request)
 
+        client = TrakerrClient("b2f59d0778004e503ddf685382297a741205333295736")
+
+        value = str(input_data)
+        value = value.lower()
+        value = value.translate(None, string.punctuation)
+        keyword = ["tylenol", "drugs", "prescription", "marajuana", "smoke"]
+        session_id = request.session.session_key
+
+        if session_id is None:
+            request.session.Create()
+            session_id = request.session.session_key
+
+        print len(session_id)
+
+        if any(x in value for x in keyword):
+            client.log({"user":"test@trakker.io", "session":session_id,
+                        "errname":"user asked a protected question",
+                        "errmessage":"User teaching my robot bad things"},
+                       "Warning", "input", False)
+
+        if "make error" in value:
+            try:
+                self._some_method()
+            except ArithmeticError:
+                client.log({"user":"test@trakker.io", "session":session_id}, "error")
+
+        if "make fatal" in value:
+            try:
+                self._some_method()
+            except ArithmeticError:
+                client.log({"user":"test@trakker.io", "session":session_id}, "fatal")
+
+        if "make info" in value:
+            client.log({"user":"test@trakker.io", "session":session_id,
+                        "errname":"chatbot info", "errmessage":"Chatbot made an info for you!"},
+                       "info")
+
+        if "make debug" in value:
+            client.log({"user":"test@trakker.io", "session":session_id,
+                        "errname":"chatbot debug",
+                        "errmessage":"Chatbot sometimes helps you debug!"},
+                       "debug")
+
         response = self.chatterbot.get_response(input_data, chat_session.id_string)
         response_data = response.serialize()
 
@@ -105,3 +156,22 @@ class ChatterBotView(ChatterBotViewMixin, View):
 
         # Return a method not allowed response
         return JsonResponse(data, status=405)
+
+
+    def _some_method(self):
+        self._another_method()
+
+    def _another_method(self):
+        self._our_code()
+
+    def _our_code(self):
+        self._is_throwing()
+
+    def _is_throwing(self):
+        self._throwing_method()
+
+    def _an_error(self):
+        self._throwing_method()
+
+    def _throwing_method(self):
+        raise ArithmeticError("Matrix Math is hard")
